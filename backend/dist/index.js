ad964736c423b56ce5a38c3b7e7faff833ddc2e9
amarouche <amarou_o@etna-alternance.net>
2021-05-21 14:03:08 +0200

    rn dist

--- backend/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const server = app_1.app.listen(3000, '127.0.0.1', () => {
-    const { port, address } = server.address();
-    console.log('Server listening on:', 'http://' + address + ':' + port);
-});
-//# sourceMappingURL=index.js.map
